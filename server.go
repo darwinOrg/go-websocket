9e1b659fcfb25ad3267110c94055d5065d5efe3f
chenbiao <149027310@qq.com>
2023-10-26 18:07:36 +0800

    init

---
+++ server.go
@@ -0,0 +1,66 @@
+package dgws
+
+import (
+	"encoding/json"
+	dglogger "github.com/darwinOrg/go-logger"
+	ve "github.com/darwinOrg/go-validator-ext"
+	"github.com/darwinOrg/go-web/utils"
+	"github.com/darwinOrg/go-web/wrapper"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		// 根据鉴权的方式来处理, 如果不想鉴权的就直接返回true, 如果需要鉴权就要根据判断来返回true，或者false
+		return true
+	},
+}
+
+func Get[T any](rh *wrapper.RequestHolder[T, error]) {
+	rh.GET(rh.RelativePath, bizHandler(rh))
+}
+
+func Post[T any](rh *wrapper.RequestHolder[T, error]) {
+	rh.POST(rh.RelativePath, bizHandler(rh))
+}
+
+func bizHandler[T any](rh *wrapper.RequestHolder[T, error]) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 服务升级，对于来到的http连接进行服务升级，升级到ws
+		cn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		defer cn.Close()
+		if err != nil {
+			panic(err)
+		}
+
+		ctx := utils.GetDgContext(c)
+		for {
+			_, message, err := cn.ReadMessage()
+			if err != nil {
+				dglogger.Errorf(ctx, "server read error: %v", err)
+				break
+			}
+			dglogger.Infof(ctx, "server receive msg: %s", message)
+
+			req := new(T)
+			err = json.Unmarshal(message, req)
+			if err != nil {
+				dglogger.Errorf(ctx, "bind message to struct error: %v", err)
+				break
+			}
+
+			if err := c.ShouldBind(req); err != nil {
+				errMsg := ve.TranslateValidateError(err, ctx.Lang)
+				dglogger.Errorf(ctx, "bind request object error: %s", errMsg)
+				break
+			}
+
+			err = rh.BizHandler(c, ctx, req)
+			if err != nil {
+				dglogger.Errorf(ctx, "biz handle message[%s] error: %v", message, err)
+			}
+		}
+	}
+}
---
+++ ws_test.go
@@ -0,0 +1,80 @@
+package dgws_test
+
+import (
+	"encoding/json"
+	"fmt"
+	dgctx "github.com/darwinOrg/go-common/context"
+	dglogger "github.com/darwinOrg/go-logger"
+	"github.com/darwinOrg/go-monitor"
+	"github.com/darwinOrg/go-web/wrapper"
+	dgws "github.com/darwinOrg/go-websocket"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+	"net/url"
+	"testing"
+	"time"
+)
+
+const path = "/echo"
+
+type Message struct {
+	Content string `json:"content" binding:"required,minLength=3"`
+}
+
+func TestSendMessage(t *testing.T) {
+	monitor.Start("test", 19002)
+	engine := wrapper.DefaultEngine()
+	dgws.Get(&wrapper.RequestHolder[Message, error]{
+		RouterGroup: engine.Group(path),
+		BizHandler: func(_ *gin.Context, ctx *dgctx.DgContext, message *Message) error {
+			dglogger.Infof(ctx, "handle message: %s", message.Content)
+			return nil
+		},
+	})
+	go engine.Run(fmt.Sprintf(":%d", 8080))
+	time.Sleep(time.Second * 3)
+
+	ctx := &dgctx.DgContext{TraceId: uuid.NewString()}
+	sendMessage(ctx, "localhost:8080", path, []Message{
+		{
+			Content: "123",
+		},
+		{
+			Content: "456",
+		},
+		{
+			Content: "789",
+		},
+		{
+			Content: "101",
+		},
+	}, 5)
+}
+
+func sendMessage(ctx *dgctx.DgContext, host string, path string, messages []Message, intervalSeconds time.Duration) {
+	u := url.URL{Scheme: "ws", Host: host, Path: path}
+	dglogger.Infof(ctx, "client connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		dglogger.Fatalf(ctx, "dial server: %v", err)
+	}
+	defer c.Close()
+
+	for _, message := range messages {
+		body, _ := json.Marshal(message)
+		err := c.WriteMessage(websocket.TextMessage, body)
+		if err != nil {
+			dglogger.Errorf(ctx, "client write error: %v", err)
+			return
+		}
+		time.Sleep(time.Second * intervalSeconds)
+	}
+
+	err = c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		dglogger.Errorf(ctx, "client write close error: %v", err)
+		return
+	}
+}
