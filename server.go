e0521f39f75ed415c9092d07a5a5fbc21cd13936
chenbiao <149027310@qq.com>
2023-12-13 02:25:58 +0800

    up

--- server.go
+++ server.go
@@ -20,6 +20,7 @@ type WebSocketMessage struct {
 	MessageData []byte
 }
 
+type GetBizIdHandler func(c *gin.Context) string
 type StartFunc func(c *gin.Context, ctx *dgctx.DgContext, conn *websocket.Conn) error
 type IsEndFunc func(mt int, data []byte) bool
 type EndCallbackFunc func(ctx *dgctx.DgContext, conn *websocket.Conn) error
@@ -115,20 +116,8 @@ func SetCheckOrigin(checkOriginFunc func(r *http.Request) bool) {
 	upgrader.CheckOrigin = checkOriginFunc
 }
 
-func GetBytes(rh *wrapper.RequestHolder[WebSocketMessage, error], startFunc StartFunc, isEndFunc IsEndFunc, endCallback EndCallbackFunc) {
-	rh.GET(rh.RelativePath, bizHandlerBytes(rh, startFunc, isEndFunc, endCallback))
-}
-
-func PostBytes(rh *wrapper.RequestHolder[WebSocketMessage, error], startFunc StartFunc, isEndFunc IsEndFunc, endCallback EndCallbackFunc) {
-	rh.POST(rh.RelativePath, bizHandlerBytes(rh, startFunc, isEndFunc, endCallback))
-}
-
-func bizHandlerBytes(rh *wrapper.RequestHolder[WebSocketMessage, error], startFunc StartFunc, isEndFunc IsEndFunc, endCallback EndCallbackFunc) gin.HandlerFunc {
-	return bizHandler(rh, startFunc, isEndFunc, endCallback)
-}
-
-func bizHandler(rh *wrapper.RequestHolder[WebSocketMessage, error], startFunc StartFunc, isEndFunc IsEndFunc, endCallback EndCallbackFunc) gin.HandlerFunc {
-	return func(c *gin.Context) {
+func GetBytes(rh *wrapper.RequestHolder[WebSocketMessage, error], bizKey string, getBizIdHandler GetBizIdHandler, startFunc StartFunc, isEndFunc IsEndFunc, endCallback EndCallbackFunc) {
+	rh.GET(rh.RelativePath, func(c *gin.Context) {
 		if semaphore != nil {
 			if !semaphore.TryAcquire() {
 				c.AbortWithStatusJSON(http.StatusOK, result.FailByDgError[dgerr.DgError](dgerr.SYSTEM_BUSY))
@@ -137,11 +126,12 @@ func bizHandler(rh *wrapper.RequestHolder[WebSocketMessage, error], startFunc St
 			defer semaphore.Release()
 		}
 		ctx := utils.GetDgContext(c)
+		bizId := getBizIdHandler(c)
 
 		// 服务升级，对于来到的http连接进行服务升级，升级到ws
 		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 		if err != nil {
-			dglogger.Errorf(ctx, "upgrade error: %v", err)
+			dglogger.Errorf(ctx, "[%s: %s] upgrade error: %v", bizKey, bizId, err)
 			return
 		}
 		SetConn(ctx, conn)
@@ -152,7 +142,7 @@ func bizHandler(rh *wrapper.RequestHolder[WebSocketMessage, error], startFunc St
 		}
 		err = startFunc(c, ctx, conn)
 		if err != nil {
-			dglogger.Errorf(ctx, "start websocket error: %v", err)
+			dglogger.Errorf(ctx, "[%s: %s] start websocket error: %v", bizKey, bizId, err)
 			WriteErrorResult(conn, err)
 			return
 		}
@@ -169,11 +159,11 @@ func bizHandler(rh *wrapper.RequestHolder[WebSocketMessage, error], startFunc St
 			mt, message, err := conn.ReadMessage()
 			if isEndFunc(mt, message) {
 				SetWsEnded(ctx)
-				dglogger.Infof(ctx, "server receive close message, error: %v", err)
+				dglogger.Infof(ctx, "[%s: %s] server receive close message, error: %v", bizKey, bizId, err)
 				if endCallback != nil {
 					err := endCallback(ctx, conn)
 					if err != nil {
-						dglogger.Errorf(ctx, "end callback error: %v", err)
+						dglogger.Errorf(ctx, "[%s: %s] end callback error: %v", bizKey, bizId, err)
 					}
 				}
 				conn.WriteMessage(websocket.CloseMessage, message)
@@ -181,7 +171,7 @@ func bizHandler(rh *wrapper.RequestHolder[WebSocketMessage, error], startFunc St
 			}
 
 			if mt == websocket.PingMessage {
-				dglogger.Info(ctx, "server receive ping message")
+				dglogger.Infof(ctx, "[%s: %s] server receive ping message", bizKey, bizId)
 				conn.WriteMessage(websocket.PongMessage, []byte("ok"))
 				continue
 			}
@@ -191,17 +181,17 @@ func bizHandler(rh *wrapper.RequestHolder[WebSocketMessage, error], startFunc St
 			}
 
 			if err != nil {
-				dglogger.Errorf(ctx, "server read error: %v", err)
+				dglogger.Errorf(ctx, "[%s: %s] server read error: %v", bizKey, bizId, err)
 				break
 			}
 
 			wsm := &WebSocketMessage{Connection: conn, MessageType: mt, MessageData: message}
 			err = rh.BizHandler(c, ctx, wsm)
 			if err != nil {
-				dglogger.Errorf(ctx, "biz handle message[%s] error: %v", message, err)
+				dglogger.Errorf(ctx, "[%s: %s] biz handle message[%s] error: %v", message, bizKey, bizId, err)
 			}
 		}
-	}
+	})
 }
 
 func WriteErrorResult(conn *websocket.Conn, err error) {
